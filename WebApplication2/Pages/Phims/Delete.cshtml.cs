de8ae65b8ee4e9e8ccfbd77def1d20a2bd939e5d
hadesTH1230 <90445479+hadesTH1230@users.noreply.github.com>
2021-10-24 13:19:38 +0700

    first commit

---
+++ WebApplication2/Pages/Phims/Delete.cshtml.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using WebApplication2.Data;
+using WebApplication2.Models;
+
+namespace WebApplication2.Pages.Phims
+{
+    public class DeleteModel : PageModel
+    {
+        private readonly WebApplication2.Data.WebApplication2Context _context;
+
+        public DeleteModel(WebApplication2.Data.WebApplication2Context context)
+        {
+            _context = context;
+        }
+
+        [BindProperty]
+        public Phim Phim { get; set; }
+
+        public async Task<IActionResult> OnGetAsync(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            Phim = await _context.Phim.FirstOrDefaultAsync(m => m.ID == id);
+
+            if (Phim == null)
+            {
+                return NotFound();
+            }
+            return Page();
+        }
+
+        public async Task<IActionResult> OnPostAsync(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            Phim = await _context.Phim.FindAsync(id);
+
+            if (Phim != null)
+            {
+                _context.Phim.Remove(Phim);
+                await _context.SaveChangesAsync();
+            }
+
+            return RedirectToPage("./Index");
+        }
+    }
+}
---
+++ WebApplication2/Pages/Phims/Details.cshtml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using WebApplication2.Data;
+using WebApplication2.Models;
+
+namespace WebApplication2.Pages.Phims
+{
+    public class DetailsModel : PageModel
+    {
+        private readonly WebApplication2.Data.WebApplication2Context _context;
+
+        public DetailsModel(WebApplication2.Data.WebApplication2Context context)
+        {
+            _context = context;
+        }
+
+        public Phim Phim { get; set; }
+
+        public async Task<IActionResult> OnGetAsync(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            Phim = await _context.Phim.FirstOrDefaultAsync(m => m.ID == id);
+
+            if (Phim == null)
+            {
+                return NotFound();
+            }
+            return Page();
+        }
+    }
+}
